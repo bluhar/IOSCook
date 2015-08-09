feb5767d5f8843efda6d3edad8f5d2ffe0de1f54
vivi 卫 <viviwei@vivideMac.local>
2015-08-10 00:36:51 +0800

    note15

---
+++ note15/Homepwner/BKDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  BKDetailViewController.h
+//  Homepwner
+//
+//  Created by vivi 卫 on 15-7-31.
+//  Copyright (c) 2015年 Colin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BKItem;
+
+@interface BKDetailViewController : UIViewController
+
+@property (nonatomic,strong) BKItem *item;
+
+@end
